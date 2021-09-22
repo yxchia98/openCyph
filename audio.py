dc06aa5ac221bfe1326e5663598183cebc130135
Lulmao <yxchia98@gmail.com>
2021-09-22 20:52:44 +0800

    encode and decode audio functions

--- audio.py
+++ audio.py
@@ -1,3 +1,5 @@
+import os.path
+import pathlib
 import sys
 
 import numpy as np
@@ -24,6 +26,14 @@ class AudioCoder:
 
 
     def encode_audio(self, source, dest, payload, bitrange):
+        # error checking, and checking if file exists
+        if source[-4:] != '.wav':
+            source += '.wav'
+        if dest[-4:] != '.wav':
+            dest += '.wav'
+        if not os.path.exists(source):
+            print('[!] Source file does not exist, only .wav files are accepted')
+            return
         song = wave.open(source, 'rb')
         print('Channels: ', song.getnchannels(), '\nSample width:', song.getsampwidth(), '\nFramerate: ',
               song.getframerate(), '\nFrames: ', song.getnframes())
@@ -36,7 +46,7 @@ class AudioCoder:
         # Convert payload to binary
         bin_payload = self.to_bin(payload)
         # apply payload padding
-        # Split the payload into specified bitranges
+        # Split the payload into specified bitrange slices
         bin_payload = [bin_payload[index: index + bitrange] for index in range(0, len(bin_payload), bitrange)]
         print(bin_payload)
 
@@ -47,18 +57,33 @@ class AudioCoder:
         bitmask = bitmask.to_bytes(1, byteorder=sys.byteorder)
         bitmask = bitmask[0]
 
+        # embed payload into frames, payload slices determined by bitrange specified
         for i, bits in enumerate(bin_payload):
+            # if binary slice is not the specified bitrange length (usually happens to the last bit), add trailing 0s
             if len(bits) < bitrange:
                 bits = bits.ljust(bitrange, '0')
+            # clear specified number of bits, and set bits according to payload
             frames[i] = (frames[i] & ~bitmask) | int(bits, 2)
+        # convert bytearray into bytes, the format to write to a wav file
         modded_frames = bytes(frames)
+        # write to a wav file on specified destination
         with wave.open(dest, 'wb') as newfile:
             newfile.setparams(song.getparams())
             newfile.writeframes(modded_frames)
             newfile.close()
         song.close()
 
-    def decode_audio(self, source, bitrange):
+    def decode_audio(self, source, dest, bitrange):
+        # error handling
+        if source[-4:] != '.wav':
+            source += '.wav'
+        if dest[-4:] != '.txt':
+            dest += '.txt'
+        if not os.path.exists(source):
+            print('[!] Source file does not exist, only .wav files are accepted')
+            return
+
+        # Read frames from specified file
         song = wave.open(source, 'rb')
         frames = song.readframes(song.getnframes())
         frames = bytearray(frames)
@@ -72,19 +97,23 @@ class AudioCoder:
             val = int(byte, 2)
             if 31 < val < 128:
                 decoded_string += format(val, 'c')
-
+        with open(dest, 'w') as newfile:
+            newfile.write(decoded_string)
+            newfile.close()
         return decoded_string
 
 
 
 if __name__ == '__main__':
     audio = AudioCoder()
-    sourcefile = './cover_assets/audio.wav'
-    destfile = './stego_assets/audio_embedded.wav'
+    source_file = './cover_assets/audio'
+    embedded_file = './stego_assets/audio_embedded'
+    decoded_text = './stego_assets/decoded_audio.txt'
+    bitrange = 1
     # payload = str(input('Enter payload to be embedded: '))
-    payload = 'THIS IS A SECRET TEXT'
-    audio.encode_audio(sourcefile, destfile, payload, 4)
-    with open('./stego_assets/decoded_audio.txt', 'w') as newfile:
-        newfile.write(audio.decode_audio(destfile, 4))
-        newfile.close()
+    payload = 'PLEASE GIVE ME A+'
+    audio.encode_audio(source_file, embedded_file, payload, bitrange)
+    audio.decode_audio(embedded_file, decoded_text, bitrange)
+
+
 
