926ab003c3cc94c900240921df7ec19d49eefb9f
Kai Heng <johnloh1998@gmail.com>
2021-09-29 13:05:09 +0800

    minor changes

--- integrate.py
+++ integrate.py
@@ -3,11 +3,13 @@ from picture import Encoder
 
 
 audiocoder = AudioCoder()
-audioBits = audiocoder.get_stream("cover_assets/audio.wav")
+# audioBits = audiocoder.get_stream("cover_assets/audio.wav")
+bits = audiocoder.get_stream("cover_assets/doge.jpg")
+print(bits)
 
-imagecoder = Encoder("./cover_assets/mountain.jpeg", "01")
-
-imagecoder.setBitNumber(4)
-imagecoder.encode(audioBits)
-# imagecoder.writeText()
-imagecoder.generateNewPic("./results./img./imgResult.png")
+# imagecoder = Encoder("./cover_assets/mountain.jpeg", "01")
+#
+# imagecoder.setBitNumber(4)
+# imagecoder.encode(audioBits)
+# # imagecoder.writeText()
+# imagecoder.generateNewPic("./results./img./imgResult.png")
